0b475dcb82c83ef0d5c86e11b6f96a18ef45e5f6
Eljefry <nahuelmamanic@gmail.com>
2024-05-28 17:27:19 -0300

    Subida de vistas de ApiRest

---
+++ backEnd/app/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+#este metodo me crea y devuelve una ruta para una carpeta con el id del usuario, es dinamico
+def GetRutaField(instancia_user,namefield):
+   return 'user_{0}/{1}'.format(instancia_user.id,namefield)
+   
+
+
+class Documentos(models.Model):
+    nombre=models.CharField(max_length=50, null=True)
+    fecha=models.DateField(null=True)
+
+    def __str__(self) -> str:
+        return self.nombre
+    
+class Usuarios(models.Model):
+    nombre=models.CharField(max_length=50)
+    email=models.EmailField(max_length=100)
+    foto=models.ImageField(null=True,blank=True,upload_to=GetRutaField)
+    contraseña= models.CharField(max_length=100)
+    certificado= models.FileField(null=False,blank=False,upload_to=GetRutaField)
+    estado = models.CharField(max_length=20, choices=(
+        (1, 'Pendiente'),
+        (2, 'Aprobado'),
+        (3, 'Rechazado'),
+    ), default='1')
+
+    rol = models.CharField(max_length=20, choices=(
+        (1, 'Estudiante'),
+        (2, 'Administrador'),
+        
+    ), default='1')
+    
+    def __str__(self):
+        return self.nombre
