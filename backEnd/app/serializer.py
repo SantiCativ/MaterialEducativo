6ca64d863aeba073c35d68d9a4f80fc3a3891f2f
Eljefry <nahuelmamanic@gmail.com>
2024-05-28 16:53:17 -0300

    creacion de vista CreateUser

--- backEnd/app/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-class AlumnosSerializers(serializers.ModelSerializer):
-    class Meta:
-        model=Alumnos
-        fields= "__all__"
--- backEnd/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from rest_framework import routers
-from app import views
-
-
-router= routers.DefaultRouter()
-router.register(r'Alumnos',views.AlumnosViewSet)
-urlpatterns= [path('',include(router.urls))]
