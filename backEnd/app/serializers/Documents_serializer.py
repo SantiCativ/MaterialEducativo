b7f0c65bb7151506dbec648d6f501568ba084203
nahuel <nahuelmamanic@gmail.com>
2024-07-27 17:13:32 -0300

    Add create document controller

--- backEnd/app/serializers/Documents_serializer.py
+++ backEnd/app/serializers/Documents_serializer.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from app.models import Documentos
 
-class DocumentSerializer(serializers.Serializer):
+class DocumentSerializer(serializers.ModelSerializer):
     class Meta:
-        Model=Documentos
-        fields="__all__"
+        model=Documentos
+        fields=["title", "created_date","file", "owner_id", "state"]
+        
+    def create(self, validated_data):
+        return Documentos.objects.create(**validated_data)
+    
+class Documents(serializers.ModelSerializer):
+    class Meta:
+        model=Documentos
+        fields='__all__'
--- backEnd/app/urls.py
+++ backEnd/app/urls.py
@@ -9,11 +9,12 @@ router= routers.DefaultRouter()
 urlpatterns= [
     path('',include(router.urls)),
     path('registro/', CreateUser.as_view(), ), 
+    path('documents_create/',CreateDocument.as_view(),),
     path('update/<int:pk>/',UpdatedUser.as_view()),
     path('delete/<int:pk>/', DeleteUser.as_view()), 
     path('user/<int:pk>/', GetUser.as_view()), 
     path('users/', GetUsers.as_view()), 
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),#esta vista recibe el usuario y contraseña y si
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
+    path('documents/', GetDocuments.as_view()), 
     ]
