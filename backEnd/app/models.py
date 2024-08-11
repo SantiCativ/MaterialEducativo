ff77f07143cf9f3d8aff14bba1333b93a6f5778b
nahuel <nahuelmamanic@gmail.com>
2024-08-11 16:43:34 -0300

    Add Score field in document model

--- backEnd/app/models.py
+++ backEnd/app/models.py
@@ -1,4 +1,4 @@
-                                                                                                                                                                                                                                                                                                                                            from django.db import models
+from django.db import models
 from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.models import AbstractUser
 
@@ -35,11 +35,23 @@ class Documentos(models.Model):
     created_date=models.DateField(auto_now=True)
     file=models.FileField(upload_to='documents',null=False,blank=False)
     owner=models.ForeignKey(Usuarios,on_delete=models.CASCADE,related_name='documents') #politica de integridad referencial(VER)
-                                                                                                                                                                            state= models.CharField(max_length=20, choices=(
+    score_sum=models.DecimalField(default=0,max_digits=10,decimal_places=2)#contiene la suma de todas las calificaciones
+    score_cant=models.IntegerField(default=0)#contiene la cantidad de calificaciones realizadas a un documento
+    state= models.CharField(max_length=20, choices=(
         (1, 'visible'),
         (2, 'oculto'),
         
     ), default='1')
+    @property#esto sirve para que la funcion average_score la pueda llamar como si fuera un campo de mi modelo, solo aplica ala funcion que tiene acontinuacion
+    def average_score(self):#me devuelve el promedio de calificaciones
+        if self.score_cant==0 :#si no tiene calificaciones devuelve 0
+            return 0
+        return self.score_sum/self.score_cant#en caso de tener calificaciones me devuelve el promedio
+    
+    def add_score(self,number):#este metodo me sirve para guardar una calficacion a un documento
+       self.score_sum+=number
+       self.score_cant+=1
+       self.save()
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.title
